ff14d92b043e860d2cd0ac3a2cab3884ade60b64
iPokeU <ianyongasd@gmail.com>
2016-09-11 23:50:35 +0800

    A bit more UI.

--- src/Simulation/Controller.java
+++ src/Simulation/Controller.java
@@ -1,4 +1,14 @@
 package Simulation;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+
 public class Controller {
+    @FXML
+    JFXButton buttonAddRouter;
+
+    @FXML
+    void setButtonAddRouter(){
+
+    }
 }
