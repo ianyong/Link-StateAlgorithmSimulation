e31d060b5345d42b9b9c56be2b212b2a85c74c43
iPokeU <ianyongasd@gmail.com>
2016-09-13 21:39:01 +0800

    Removed border around node.

--- src/Simulation/Controller.java
+++ src/Simulation/Controller.java
@@ -2,13 +2,17 @@ package Simulation;
 
 import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
 
 public class Controller {
+
     @FXML
     JFXButton buttonAddRouter;
+    @FXML
+    Pane pane;
 
     @FXML
     private void addNode(){
-        //TODO
+
     }
 }
