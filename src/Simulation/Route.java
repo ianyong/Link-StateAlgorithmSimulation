5df511d1c7c2e6ded360c8ef6fb852e96e54fe69
Yusiang <yusiang1998@gmail.com>
2016-09-13 21:37:46 +0800

    Simulator work

--- src/Simulation/Route.java
+++ src/Simulation/Route.java
@@ -5,14 +5,19 @@ package Simulation;
  */
 public class Route {
     private double x1,y1,x2,y2;
-    private Node n1,n2;
+    protected final Node n1,n2;
+    private double weight;
 
-    public Route(Node n1, Node n2) {
+    public Route(Node n1, Node n2, double weight) {
+        this.n1 = n1;
+        this.n2 = n2;
+        updateXY();
+        this.weight = weight;
+    }
+    public void updateXY(){
         this.x1 = n1.x;
         this.y1 = n1.y;
         this.x2 = n2.x;
         this.y2 = n2.y;
-        this.n1 = n1;
-        this.n2 = n2;
     }
 }
--- src/Simulation/Simulator.java
+++ src/Simulation/Simulator.java
@@ -1,7 +1,19 @@
 package Simulation;
 
+import java.util.ArrayList;
+
 /**
  * Created by yusiang on 13/9/16.
  */
 public class Simulator {
+    private ArrayList<Node> nodes = new ArrayList<Node>();
+    private ArrayList<Route> routes = new ArrayList<Route>();
+
+    public ArrayList<Route> getMST(Node origin){
+        ArrayList<Route> mst = new ArrayList<Route>();
+        ArrayList<Node> unfoundNodes = new ArrayList<Node>(nodes);//Copy of nodes. Will remove once connected.
+        if(!nodes.contains(origin)) return null;//WTF
+
+        return mst;
+    }
 }
